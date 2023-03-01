4b17d1308f77b72e7a3677c1f295cc465e12cc98
TusharSNagpal <nagpal8795@gmail.com>
2023-03-01 14:09:58 +0530

    css-separate

--- ionic-react-frontend/src/components/Doctor/DoctorHome.tsx
+++ ionic-react-frontend/src/components/Doctor/DoctorHome.tsx
@@ -27,6 +27,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import '../../theme/variables.css';
+import './Doctor.css';
 import {useEffect, useState} from "react";
 
 // import {Redirect} from "react-router";
